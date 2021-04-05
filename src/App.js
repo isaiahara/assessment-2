c0bea39d5c506c6dde3d76554abe9eb22beefc5a
isaiah aragon <isaiahara87@gmail.com>
2021-04-05 16:01:31 -0600

    assessment 2, found that i dont know much

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Modal, Card } from "react-bootstrap";
+
+function list() {
+  return (
+  
+    <Card className="list">
+      <div className="list">
+        
+        <Card.Body>
+         
+          <Button> variant="primary" </Button>
+        </Card.Body>
+      </div>
+    </Card>
+  );
+}
+
+export default React;
+// epic fail need to do some soul searching
